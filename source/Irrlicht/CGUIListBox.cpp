Venkateshbodem/gitlab_migration_repo

commit 7aedf5a9bdf646cb45cb6628ef23b00a64a78eba

Fix: Listbox was sometimes sending EGET_LISTBOX_SELECTED_AGAIN instead of EGET_LISTBOX_CHANGED. When pressed mouse was moved over an item before releasing the mouse button it was sending immediately EGET_LISTBOX_SELECTED_AGAIN instead of expected EGET_LISTBOX_CHANGED (mouse  move changes do not send any events).
git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@6454 dfc29bdd-3216-0410-991c-e03cc46cb475

source/Irrlicht/CGUIListBox.cpp
@@ -23,10 +23,10 @@ namespace gui
 CGUIListBox::CGUIListBox(IGUIEnvironment* environment, IGUIElement* parent,
 			s32 id, core::rect<s32> rectangle, bool clip,
 			bool drawBack, bool moveOverSelect)
-: IGUIListBox(environment, parent, id, rectangle), Selected(-1),
+: IGUIListBox(environment, parent, id, rectangle), Selected(-1), HoverSelected(-1),
 	ItemHeight(0),ItemHeightOverride(0),
 	TotalItemHeight(0), ItemsIconWidth(0), Font(0), IconBank(0),
-	ScrollBar(0), selectTime(0), LastKeyTime(0), Selecting(false), DrawBack(drawBack),
+	ScrollBar(0), SelectTime(0), LastKeyTime(0), Selecting(false), DrawBack(drawBack),
 	MoveOverSelect(moveOverSelect), AutoScroll(true), HighlightWhenNotFocused(true)
 {
 	#ifdef _DEBUG
@@ -110,7 +110,7 @@ void CGUIListBox::removeItem(u32 id)
 	else if ((u32)Selected > id)
 	{
 		Selected -= 1;
-		selectTime = os::Timer::getTime();
+		SelectTime = os::Timer::getTime();
 	}
 
 	Items.erase(id);
@@ -142,6 +142,7 @@ void CGUIListBox::clear()
 	Items.clear();
 	ItemsIconWidth = 0;
 	Selected = -1;
+	HoverSelected = -1;
 
 	ScrollBar->setPos(0);
 
@@ -186,7 +187,7 @@ void CGUIListBox::recalculateItemHeight()
 //! returns id of selected item. returns -1 if no item is selected.
 s32 CGUIListBox::getSelected() const
 {
-	return Selected;
+	return HoverSelected >= 0 ? HoverSelected : Selected;
 }
 
 
@@ -198,7 +199,8 @@ void CGUIListBox::setSelected(s32 id)
 	else
 		Selected = id;
 
-	selectTime = os::Timer::getTime();
+	HoverSelected = -1;
+	SelectTime = os::Timer::getTime();
 
 	recalculateScrollPos();
 }
@@ -448,14 +450,19 @@ void CGUIListBox::selectNew(s32 ypos, bool onlyHover)
 	u32 now = os::Timer::getTime();
 	s32 oldSelected = Selected;
 
-	Selected = getItemAt(AbsoluteRect.UpperLeftCorner.X, ypos);
-	if (Selected<0 && !Items.empty())
-		Selected = 0;
+	HoverSelected = getItemAt(AbsoluteRect.UpperLeftCorner.X, ypos);
+	if (HoverSelected<0 && !Items.empty())
+		HoverSelected = 0;
+	if  (!onlyHover)
+	{
+		Selected = HoverSelected;
+		HoverSelected = -1;
+	}
 
 	recalculateScrollPos();
 
-	gui::EGUI_EVENT_TYPE eventType = (Selected == oldSelected && now < selectTime + 500) ? EGET_LISTBOX_SELECTED_AGAIN : EGET_LISTBOX_CHANGED;
-	selectTime = now;
+	gui::EGUI_EVENT_TYPE eventType = (Selected == oldSelected && now < SelectTime + 500) ? EGET_LISTBOX_SELECTED_AGAIN : EGET_LISTBOX_CHANGED;
+	SelectTime = now;
 	// post the news
 	if (Parent && !onlyHover)
 	{
@@ -521,13 +528,14 @@ void CGUIListBox::draw()
 	frameRect.LowerRightCorner.Y -= ScrollBar->getPos();
 
 	bool hl = (HighlightWhenNotFocused || Environment->hasFocus(this) || Environment->hasFocus(ScrollBar));
+	const irr::s32 selected = getSelected();
 
 	for (s32 i=0; i<(s32)Items.size(); ++i)
 	{
 		if (frameRect.LowerRightCorner.Y >= AbsoluteRect.UpperLeftCorner.Y &&
 			frameRect.UpperLeftCorner.Y <= AbsoluteRect.LowerRightCorner.Y)
 		{
-			if (i == Selected && hl)
+			if (i == selected && hl)
 			{
 				skin->draw2DRectangle(this, hasItemOverrideColor(i, EGUI_LBC_BACKGROUND_HIGHLIGHT) ? getItemOverrideColor(i, EGUI_LBC_BACKGROUND_HIGHLIGHT) : getItemDefaultColor(EGUI_LBC_BACKGROUND_HIGHLIGHT), frameRect, &clientClip);
 			}
@@ -547,24 +555,24 @@ void CGUIListBox::draw()
 					iconPos.Y += textRect.getHeight() / 2;
 					iconPos.X += ItemsIconWidth/2;
 
-					if ( i==Selected && hl )
+					if ( i==selected && hl )
 					{
 						IconBank->draw2DSprite( (u32)Items[i].Icon, iconPos, &clientClip,
 							hasItemOverrideColor(i, EGUI_LBC_ICON_HIGHLIGHT) ?
 							getItemOverrideColor(i, EGUI_LBC_ICON_HIGHLIGHT) : getItemDefaultColor(EGUI_LBC_ICON_HIGHLIGHT),
-							selectTime, os::Timer::getTime(), false, true);
+							SelectTime, os::Timer::getTime(), false, true);
 					}
 					else
 					{
 						IconBank->draw2DSprite( (u32)Items[i].Icon, iconPos, &clientClip,
 							hasItemOverrideColor(i, EGUI_LBC_ICON) ? getItemOverrideColor(i, EGUI_LBC_ICON) : getItemDefaultColor(EGUI_LBC_ICON),
-							0 , (i==Selected) ? os::Timer::getTime() : 0, false, true);
+							0 , (i==selected) ? os::Timer::getTime() : 0, false, true);
 					}
 				}
 
 				textRect.UpperLeftCorner.X += ItemsIconWidth+3;
 
-				if ( i==Selected && hl )
+				if ( i==selected && hl )
 				{
 					Font->draw(Items[i].Text.c_str(), textRect,
 						hasItemOverrideColor(i, EGUI_LBC_TEXT_HIGHLIGHT) ?
@@ -768,6 +776,7 @@ void CGUIListBox::deserializeAttributes(io::IAttributes* in, io::SAttributeReadW
 		}
 	}
 	Selected = in->getAttributeAsInt("Selected", Selected);
+	HoverSelected = -1;
 	recalculateScrollPos();
 }
 

source/Irrlicht/CGUIListBox.h
@@ -164,6 +164,7 @@ namespace gui
 
 		core::array< ListItem > Items;
 		s32 Selected;
+		s32 HoverSelected;	// When >= 0 we're in the middle of changing selection while mouse is pressed. We need to know so selected again isn't called too often.
 		s32 ItemHeight;
 		s32 ItemHeightOverride;
 		s32 TotalItemHeight;
@@ -171,7 +172,7 @@ namespace gui
 		gui::IGUIFont* Font;
 		gui::IGUISpriteBank* IconBank;
 		gui::IGUIScrollBar* ScrollBar;
-		u32 selectTime;
+		u32 SelectTime;
 		u32 LastKeyTime;
 		core::stringw KeyBuffer;
 		bool Selecting;

commit 0046d6c74730003afee2f53e80e9111614f6b1bd

Fix recently broken scroll behaviour of listbox with moveOverSelect This also fixes the combobox which users the listbox like that. This got broken in [r6454] which had fixed the events send out by the listbox Note: Still a bit strange behaviour when leaving the combobox at the bottom, but that was already that way in Irrlicht 1.8, so I've got to investigate that on it's own.
git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@6463 dfc29bdd-3216-0410-991c-e03cc46cb475

source/Irrlicht/CGUIListBox.cpp
@@ -631,7 +631,7 @@ void CGUIListBox::recalculateScrollPos()
 	if (!AutoScroll)
 		return;
 
-	const s32 selPos = (Selected == -1 ? TotalItemHeight : Selected * ItemHeight) - ScrollBar->getPos();
+	const s32 selPos = (getSelected() == -1 ? TotalItemHeight : getSelected() * ItemHeight) - ScrollBar->getPos();
 
 	if (selPos < 0)
 	{